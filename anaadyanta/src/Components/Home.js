28aee2265a1e3bb2012ab16aa66e8b13846f9e34
Ashwin <ashwinrb74@gmail.com>
2023-04-22 01:32:26 +0530

    SVG Animation

--- anaadyanta/src/Components/Home.js
+++ anaadyanta/src/Components/Home.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import videoBg from "../Assets/videoBg1.mp4";
-import Svg from "./Svg";
+import Logo from "./Logo";
+import Aylogo from "./Aylogo";
+import Desclogo from "./Desclogo";
+import Date from "./Date";
 import "../Styles/Home.css";
 
 export class Home extends Component {
   render() {
     return (
-      <div className="vid" id="home">
+      <div className="vid" id="Home">
         <video className="vid" src={videoBg} autoPlay loop muted />
         <div className="content">
-          <Svg />
+          <Logo />
+          <Aylogo/>
+          <Desclogo />
           <h4>An Annual National Level Techno-Cultural Event</h4>
+          <Date />
         </div>
       </div>
     );
